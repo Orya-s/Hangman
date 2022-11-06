fedd7baf49a6ed81b2697b13422bc21e51eeba1d
orya <orya.sp@gmail.com>
2022-11-06 21:45:18 +0200

    Render solution and hint in Solution component

--- src/components/solution.js
+++ src/components/solution.js
@@ -1,13 +1,28 @@
 /* This component will display empty spaces ( _ _ _ _ ) representing the letters the user has to guess.
 It will also display the hint for the word to be guessed. */ 
- 
+import React, { Component } from 'react';
+import Letter from './Letter';
+
 class Solutioin extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
     }
-    state = {  }
+    
     render() { 
-        return (  );
+        let solution = this.props.solution.word.split("");
+        let letterStatus = this.props.letterStatus;
+        let uniqeKey = 0;
+
+        return (
+            <div>
+                <div>
+                    {solution.map(w => letterStatus[w] ? <Letter key={w} data={w}/> : <Letter key={uniqeKey++} data={'__ '} />)}
+                </div>
+                <div>
+                    {this.props.solution.hint}
+                </div>
+            </div>
+        );
     }
 }
  
